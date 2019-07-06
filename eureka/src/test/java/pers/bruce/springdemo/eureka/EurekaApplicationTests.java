723e0dff0ff7896fbdad8090bbc9fbb1263dd092
iamtkjxlh <iamtkjxlh@163.com>
2019-07-06 08:25:48 +0800

    first commit

---
+++ eureka/src/test/java/pers/bruce/springdemo/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package pers.bruce.springdemo.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
